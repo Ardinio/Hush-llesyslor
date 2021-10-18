70ae8c0097cd95dfbecffc402e754e12a037c328
millbergm <83matmil@skola.boras.se>
2021-10-18 13:24:22 +0200

    added logo to loginScreen + styling on input boxes, added modal on registration

--- styles/Styles.ts
+++ styles/Styles.ts
@@ -7,6 +7,57 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        // justifyContent: 'center'
+    },
+    buttonsContainer: {
+        bottom: 10,
+        flexDirection: "row",
+        backgroundColor: "transparent",
+        position: 'absolute',
+        width: "100%"
+      },
+    button: {
+        backgroundColor: "#8E9080",
+        padding: 2,
+        margin: 2,
+        borderRadius: 20,
+        width: "50%"
+    },
+    buttonText: {
+        textAlign: "center",
+        fontWeight: "600"
+    },
+    clickableText: {
+        color: "steelblue",
+        textAlign: "center",
+        fontWeight: "600"        
+    },
+    modalView: {
+        width: "70%",
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      textInputBox: {
+        width: 200,
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 5,
+        padding: 5,
+        margin: 5,
+      },
+      loginLogo: {
+        marginTop: 50,
+        marginBottom: 50,
+      },
 })

6a563a64964914dcea455a1d94bd231f7ff6235d
Ardi Grapci <ardiehnberggrapci@gmail.com>
2021-10-19 14:49:33 +0200

    Moved the stylning to the style file

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import { styles } from "../styles/Styles";
 
 const TaskCard = () => {
 
@@ -11,26 +12,5 @@ const TaskCard = () => {
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    item: {
-        backgroundColor: "#FFF",
-        padding: 10,
-        borderRadius: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 10,
-    },
-    itemLeft: {
-        flexDirection: "row",
-        alignItems: 'center',
-        flexWrap: "wrap",
-    },
-    itemText: {
-        fontSize: 14,
-        fontWeight: "bold",
-    },
-})
 
 export default TaskCard;
--- styles/Styles.ts
+++ styles/Styles.ts
@@ -8,5 +8,23 @@ export const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    }
+    },
+    item: {
+        backgroundColor: "#FFF",
+        padding: 10,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+    itemLeft: {
+        flexDirection: "row",
+        alignItems: 'center',
+        flexWrap: "wrap",
+    },
+    itemText: {
+        fontSize: 14,
+        fontWeight: "bold",
+    },
 })

2deeceee2a547ea61fbd0943071a9afe2d897b4c
Kristina <71016430+74kricap@users.noreply.github.com>
2021-10-20 11:04:14 +0200

    Added a list of avatars

---
+++ components/Avatar.tsx
@@ -0,0 +1,48 @@
+export interface Avatar {
+    id: number;
+    emoji: string;
+    color: string;
+    // isAvailable: boolean;
+}
+
+export const Avatar = [{
+    id: '1',
+    emoji: '🦊',
+    color: '#ffa71b',
+},
+{
+    id: '2',
+    emoji: '🐥',
+    color: '#ffff00',
+},
+{
+    id: '3',
+    emoji: '🐋',
+    color: '#3100ff',
+},
+{
+    id: '4',
+    emoji: '🐷',
+    color: '#ff99cc',
+},
+{
+    id: '5',
+    emoji: '🐸',
+    color: '#c5f404',
+},
+{
+    id: '6',
+    emoji: '🐶',
+    color: '#876635',
+},
+{
+    id: '7',
+    emoji: '🐭',
+    color: '#848484',
+},
+{
+    id: '8',
+    emoji: '🦄',
+    color: '#aec9fe',
+},
+]

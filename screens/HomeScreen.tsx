20a963160f189cac5213087f1ff58e13bbf9d095
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-20 10:26:57 +0200

    added touchableOpacity to flatlistitem

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import { View, Text, Alert, FlatList } from 'react-native';
-import { styles } from '../styles/Styles';
-import { Button } from '../components';
-import { GenericScreenProps } from '../navigation/AppStack';
-import { useAppSelector, useAppDispatch } from '../store/store';
-import { selectAllHouseholds } from '../store/household/householdSelectors';
-import { AddHousehold } from '../store/household/householdActions';
-import TaskCard from '../components/TaskCard';
-import { TouchableRipple } from 'react-native-paper';
-import { tasks } from '../data/taskData';
+import * as React from "react";
+import { View, Text, Alert, FlatList } from "react-native";
+import { styles } from "../styles/Styles";
+import { Button } from "../components";
+import { GenericScreenProps } from "../navigation/AppStack";
+import { useAppSelector, useAppDispatch } from "../store/store";
+import { selectAllHouseholds } from "../store/household/householdSelectors";
+import { AddHousehold } from "../store/household/householdActions";
+import TaskCard from "../components/TaskCard";
+import { TouchableRipple } from "react-native-paper";
+import { tasks } from "../data/taskData";
+import { TouchableHighlight as TouchableOpacity } from "react-native-gesture-handler";
 
 type Props = GenericScreenProps<"HomeScreen">;
 
@@ -17,26 +18,33 @@ function HomeScreen({ navigation }: Props) {
   const dispatch = useAppDispatch();
 
   const handleAdd = () => {
-    dispatch(AddHousehold({Id: 1, Name: 'household 1', GeneratedCode: '123'}));
-    Alert.alert('Added new household');
-  }
+    dispatch(
+      AddHousehold({ Id: 1, Name: "household 1", GeneratedCode: "123" })
+    );
+    Alert.alert("Added new household");
+  };
 
   const handlePrint = () => {
-    Alert.alert('Print (see console)');
-    console.log('allHouseholds: ', allHouseholds);
-  }
+    Alert.alert("Print (see console)");
+    console.log("allHouseholds: ", allHouseholds);
+  };
   return (
     <View style={styles.container}>
-     <FlatList
+      <FlatList
         data={tasks}
         renderItem={({ item }: any) => (
-            <TaskCard
-              task={item}
-            />
+          <TouchableOpacity
+            onPress={() =>
+              console.log(item, "öppna modul för att läsa mer om tasken")
+              
+            }
+            underlayColor='none'
+          >
+            <TaskCard 
+            task={item} />
+          </TouchableOpacity>
         )}
       />
-
-
 
       <Text>Home Screen</Text>
       <Button
@@ -44,10 +52,7 @@ function HomeScreen({ navigation }: Props) {
         btnType="plus-circle"
         onPress={handleAdd}
       />
-      <Button buttonTitle="Print" 
-      btnType="print"
-      onPress={handlePrint}
-      />
+      <Button buttonTitle="Print" btnType="print" onPress={handlePrint} />
     </View>
   );
 }

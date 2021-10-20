4648acdbd0736dfe785025226f7b99f728681ed4
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-20 13:38:35 +0200

    Added modal

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Alert, FlatList } from "react-native";
+import { View, Text, Alert, FlatList, Modal } from "react-native";
 import { styles } from "../styles/Styles";
 import { Button } from "../components";
 import { GenericScreenProps } from "../navigation/AppStack";
@@ -7,15 +7,17 @@ import { useAppSelector, useAppDispatch } from "../store/store";
 import { selectAllHouseholds } from "../store/household/householdSelectors";
 import { AddHousehold } from "../store/household/householdActions";
 import TaskCard from "../components/TaskCard";
-import { TouchableRipple } from "react-native-paper";
 import { tasks } from "../data/taskData";
 import { TouchableHighlight as TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react";
+import { MaterialIcons } from '@expo/vector-icons' 
 
 type Props = GenericScreenProps<"HomeScreen">;
 
 function HomeScreen({ navigation }: Props) {
   const allHouseholds = useAppSelector(selectAllHouseholds);
   const dispatch = useAppDispatch();
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleAdd = () => {
     dispatch(
@@ -30,12 +32,30 @@ function HomeScreen({ navigation }: Props) {
   };
   return (
     <View style={styles.container}>
+      <Modal 
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible} 
+       >
+        <View style={styles.container}>
+          <View style={styles.modalView}>
+        <MaterialIcons 
+      name='close'
+      size={24}
+      onPress={() => setModalVisible(!modalVisible)}/>
+
+          <Text>
+            Hello from modal
+          </Text>
+        </View>
+        </View>
+        </Modal>
       <FlatList
         data={tasks}
         renderItem={({ item }: any) => (
           <TouchableOpacity
             onPress={() =>
-              console.log(item, "öppna modul för att läsa mer om tasken")
+              setModalVisible(true)
               
             }
             underlayColor='none'
--- styles/Styles.ts
+++ styles/Styles.ts
@@ -94,5 +94,22 @@ export const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: 'space-between',
         margin: 15,
-    }
+    },
+    modalView: {
+        width: "70%",
+        height: 200,
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
 })

61d03812cb503283fcdb48b0858f3344fe645b41
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-21 14:06:58 +0200

    cleaned code

--- components/HouseholdCard.tsx
+++ components/HouseholdCard.tsx
@@ -1,27 +1,20 @@
 import * as React from "react";
-import { FC } from "react";
-import { View, Text } from "react-native";
-import { Card, List } from "react-native-paper";
+import { Text } from "react-native";
+import { Card } from "react-native-paper";
 import { Household } from "../entities/Household";
-import { User } from "../entities/User";
-import { selectAllHouseholds } from "../store/household/householdSelectors";
-import { useAppDispatch, useAppSelector } from "../store/store";
-import { selectAllUsers } from "../store/user/userSelectors";
 import { styles } from "../styles/Styles";
 
 interface Props {
-    household: Household;
-
+  household: Household;
 }
 
 const HouseholdCard = ({ household }: Props) => {
-    
-    return (
+  return (
     <Card style={styles.Card}>
-       
-        <Text style={styles.title}>{household?.Name}</Text>
-        <Text style={styles.italicFont}>Kod för att gå med i hushållet: {household?.GeneratedCode}</Text>
-      
+      <Text style={styles.title}>{household?.Name}</Text>
+      <Text style={styles.italicFont}>
+        Kod för att gå med i hushållet: {household?.GeneratedCode}
+      </Text>
     </Card>
   );
 };

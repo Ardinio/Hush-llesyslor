da547f276abf8c8024357cee75d4b604912f0f26
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-10-25 15:08:20 +0200

    - Added selectors to get total work done by users and to get specific tasks. Used in pie charts for statistics.

--- screens/StatisticsScreen.tsx
+++ screens/StatisticsScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import { View, Text, Button, Alert, ScrollView } from 'react-native';
 import { useAppSelector, useAppDispatch } from '../store/store';
 import { selectAllHouseholds } from '../store/household/householdSelectors';
 import { AddHousehold } from '../store/household/householdActions';
@@ -7,7 +7,7 @@ import { selectAllUsers, selectUserById } from '../store/user/userSelectors';
 import { AddUser } from '../store/user/userActions';
 import { selectAllTasks } from '../store/task/taskSelectors';
 import { AddTask } from '../store/task/taskActions';
-import { selectAllCompletedTasks, selectCompletedTasksByDate } from '../store/completedtask/completedtaskSelectors';
+import { selectAllCompletedTasks, selectCompletedTasksTotalByDate, selectCompletedTasksByTask } from '../store/completedtask/completedtaskSelectors';
 import { AddCompletedTask } from '../store/completedtask/completedtaskActions';
 import { ChartPie, PieChartInputData } from '../components/ChartPie';
 import { singleAvatarById } from '../data/avatars';
@@ -15,43 +15,14 @@ import { singleUserById, singleTaskById } from '../data/selectSingleObject';
 
 function StatisticsScreen() {
   const dispatch = useAppDispatch();
-  const allHouseholds = useAppSelector(selectAllHouseholds);
-  const allUsers = useAppSelector(selectAllUsers);
-  const allTasks = useAppSelector(selectAllTasks);
-  const allCompletedTasks = useAppSelector(selectAllCompletedTasks);
 
   const currentDate: Date = new Date();
   console.log('currentDate: ', currentDate);        // Debug.
   const lastWeek: Date = new Date(+currentDate);
   lastWeek.setDate(lastWeek.getDate() - 7);
   console.log('lastWeek: ', lastWeek);              // Debug.
-  const allCompletedTasksByDate = useAppSelector(selectCompletedTasksByDate(lastWeek, currentDate));
-
-  // const chartPieData:PieChartInputData[] = [{avatarId: 7, color: '#00ff00', energy: 10}, {avatarId: 2, color: '#ff0000', energy: 3}, {avatarId: 3, color: '#0000ff', energy: 5}];
-
-  const totalChartData = ():PieChartInputData[] => {
-    let chartPieData:PieChartInputData[] = [];
-    // console.log('allCompletedTasksByDate: ', allCompletedTasksByDate);            // Debug.
-    allCompletedTasksByDate.forEach((element) => {
-      console.log('allCompletedTasksByDate element: ', element);
-      const user = singleUserById(allUsers, element.UserId);
-      const avatar = singleAvatarById(user.AvatarId);
-      const task = singleTaskById(allTasks, element.UserId);
-      const pieObject: PieChartInputData = {avatarId: user.AvatarId, color: avatar.Color, energy: task.EnergyRequired}
-      chartPieData.push(pieObject);
-      if(chartPieData.length > 0) {
-        chartPieData.forEach((element2) => {
-          if(element2.avatarId !== avatar.Id) {
-            console.log('user: ', user, ' avatar: ', avatar);
-          }
-          else
-            console.log('SAME')
-        })
-      }
-    });
-    console.log('chartPieData: ', chartPieData);            // Debug
-    return chartPieData;
-  }
+  const allCompletedTasksByDateTotal = useAppSelector(selectCompletedTasksTotalByDate(lastWeek, currentDate));
+  const allCompletedTasksByDateByTask = useAppSelector(selectCompletedTasksByTask(lastWeek, currentDate));
 
   const handleAddMock = () => {
     dispatch(AddHousehold({Id: 1, Name: 'household 1', GeneratedCode: '123'}));
@@ -61,38 +32,35 @@ function StatisticsScreen() {
     dispatch(AddUser({Id: 3, AccountId: 3, HouseholdId: 1, Name: 'user 3', AvatarId: 4, IsOwner: false}));
     dispatch(AddUser({Id: 4, AccountId: 4, HouseholdId: 2, Name: 'user 4', AvatarId: 5, IsOwner: true}));
     const date:Date = new Date();
-    const date1:Date = new Date('2021-10-15T12:00:00');
-    const date2:Date = new Date('2021-10-01T12:00:00');
-    const date3:Date = new Date('2021-10-19T12:00:00');
-    const date4:Date = new Date('2021-10-18T12:00:00');
     dispatch(AddTask({Id: 1, HouseholdId: 1, Title: 'task 1', Description: 'description 1', LastCheckDate: date, DaysToComplete: 3, EnergyRequired: 3})); 
     dispatch(AddTask({Id: 2, HouseholdId: 1, Title: 'task 2', Description: 'description 2', LastCheckDate: date, DaysToComplete: 2, EnergyRequired: 2})); 
     dispatch(AddTask({Id: 3, HouseholdId: 1, Title: 'task 3', Description: 'description 3', LastCheckDate: date, DaysToComplete: 1, EnergyRequired: 1})); 
+    const date1:Date = new Date('2021-10-15T12:00:00');
+    const date2:Date = new Date('2021-10-23T12:00:00');
+    const date3:Date = new Date('2021-10-21T12:00:00');
+    const date4:Date = new Date('2021-10-22T12:00:00');
     dispatch(AddCompletedTask({Id: 1, TasksId: 1, UserId: 1, CompleteDate: date1}));
     dispatch(AddCompletedTask({Id: 2, TasksId: 2, UserId: 2, CompleteDate: date2}));
     dispatch(AddCompletedTask({Id: 3, TasksId: 3, UserId: 3, CompleteDate: date3}));
     dispatch(AddCompletedTask({Id: 4, TasksId: 3, UserId: 3, CompleteDate: date4}));
-    dispatch(AddCompletedTask({Id: 4, TasksId: 2, UserId: 3, CompleteDate: date4}));
-  }
-
-  const handlePrint = () => {
-    // Alert.alert('Print (see console)');
-    // console.log('allHouseholds: ', allHouseholds);
-    // console.log('allUsers: ', allUsers);
-    // console.log('allTasks: ', allTasks);
-    // console.log('allCompletedTasks: ', allCompletedTasks);
-    // console.log('allCompletedTasksByDate: ', allCompletedTasksByDate);
-    // allCompletedTasksByDate.forEach((element, index, array) => {console.log('element: ', element, ' index: ', index)});
-    totalChartData();
+    dispatch(AddCompletedTask({Id: 5, TasksId: 2, UserId: 3, CompleteDate: date4}));
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Statistics Screen</Text>
-      <Button title="Mock add" onPress={handleAddMock}></Button>
-      <Button title="Mock print" onPress={handlePrint}></Button>
-      <ChartPie data={totalChartData()} />
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Statistics Screen</Text>
+        <Button title="Mock add" onPress={handleAddMock}></Button>
+        <Text>Total</Text>
+        <ChartPie data={allCompletedTasksByDateTotal} />
+        {allCompletedTasksByDateByTask.map((value) => (
+          <View>
+            <Text>Task id: {value.TaskId}</Text>
+            <ChartPie data={value.pieChartArray} />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
--- store/completedtask/completedtaskSelectors.ts
+++ store/completedtask/completedtaskSelectors.ts
@@ -1,7 +1,62 @@
+import { PieChartInputData } from '../../components/ChartPie';
 import { RootState } from '../store';
+import { singleAvatarById } from '../../data/avatars';
 
 export const selectAllCompletedTasks = (state: RootState) => state.completedtask.AllCompletedTasks;
-export const selectCompletedTasksByDate = (startdate: Date, enddate: Date) => (state: RootState) => {
-  console.log('startdate: ', startdate.getTime());
-  return state.completedtask.AllCompletedTasks.filter((x) => { return x.CompleteDate.getTime() > startdate.getTime() && x.CompleteDate.getTime() < enddate.getTime() });
+export const selectCompletedTasksTotalByDate = (startdate: Date, enddate: Date) => (state: RootState) => {
+  const byDate = state.completedtask.AllCompletedTasks.filter((x) => { return x.CompleteDate.getTime() > startdate.getTime() && x.CompleteDate.getTime() < enddate.getTime() });
+  const pieChartArray: PieChartInputData[] = [];
+  byDate.forEach((value) => {
+    const user = state.user.AllUsers.find((x) => x.Id === value.UserId) ?? { Id: -1, AccountId: -1, HouseholdId: -1, Name: '', AvatarId: -1, IsOwner: false };
+    const task = state.task.AllTasks.find((x) => x.Id === value.TasksId) ?? { Id: -1, HouseholdId: -1, Title: '', Description: '', LastCheckDate: new Date(-1), DaysToComplete: -1, EnergyRequired: -1 };
+    const avatar = singleAvatarById(user.AvatarId);
+    const returned:number = pieChartArray.findIndex((x) => x.avatarId === user.AvatarId);
+    if (returned >= 0) {
+      pieChartArray[returned].energy = pieChartArray[returned].energy + task.EnergyRequired;
+    }
+    else {
+      const pieObject: PieChartInputData = { avatarId: user.AvatarId, color: avatar.Color, energy: task.EnergyRequired }
+      pieChartArray.push(pieObject);
+    }
+  });
+  return pieChartArray;
+}
+
+export type containerArray = {
+  TaskId: number,
+  pieChartArray: PieChartInputData[]
+}
+
+export const selectCompletedTasksByTask = (startdate: Date, enddate: Date) => (state: RootState) => {
+  console.log('startdate: ', startdate.getTime(), ' enddate: ', enddate.getTime());
+  const arrayContainer: containerArray[] = [];
+  const byDate = state.completedtask.AllCompletedTasks.filter((x) => { return x.CompleteDate.getTime() > startdate.getTime() && x.CompleteDate.getTime() < enddate.getTime() });
+  byDate.forEach((value) => {
+    const indexFromContainer: number = arrayContainer.findIndex((x) => x.TaskId === value.TasksId);
+    if (indexFromContainer >= 0) {
+      const user = state.user.AllUsers.find((x) => x.Id === value.UserId) ?? { Id: -1, AccountId: -1, HouseholdId: -1, Name: '', AvatarId: -1, IsOwner: false };
+      const task = state.task.AllTasks.find((x) => x.Id === value.TasksId) ?? { Id: -1, HouseholdId: -1, Title: '', Description: '', LastCheckDate: new Date(-1), DaysToComplete: -1, EnergyRequired: -1 };
+      const indexFromPieChart: number = arrayContainer[indexFromContainer].pieChartArray.findIndex((x) => x.avatarId === user.AvatarId);
+      if (indexFromPieChart >= 0) {
+        arrayContainer[indexFromContainer].pieChartArray[indexFromPieChart].energy = arrayContainer[indexFromContainer].pieChartArray[indexFromPieChart].energy + task.EnergyRequired;
+      }
+      else {
+        const avatar = singleAvatarById(user.AvatarId);
+        const pieObject: PieChartInputData = { avatarId: user.AvatarId, color: avatar.Color, energy: task.EnergyRequired }
+        arrayContainer[indexFromContainer].pieChartArray.push(pieObject);
+      }
+    }
+    else {
+      const pieChartArrayToPush: PieChartInputData[] = [];
+      const taskId: number = value.TasksId;
+      const user = state.user.AllUsers.find((x) => x.Id === value.UserId) ?? { Id: -1, AccountId: -1, HouseholdId: -1, Name: '', AvatarId: -1, IsOwner: false };
+      const task = state.task.AllTasks.find((x) => x.Id === value.TasksId) ?? { Id: -1, HouseholdId: -1, Title: '', Description: '', LastCheckDate: new Date(-1), DaysToComplete: -1, EnergyRequired: -1 };
+      const avatar = singleAvatarById(user.AvatarId);
+      const pieObject: PieChartInputData = { avatarId: user.AvatarId, color: avatar.Color, energy: task.EnergyRequired }
+      pieChartArrayToPush.push(pieObject);
+      const object2: containerArray = {TaskId: taskId, pieChartArray: pieChartArrayToPush}
+      arrayContainer.push(object2)
+    }
+  });
+  return arrayContainer;
 }

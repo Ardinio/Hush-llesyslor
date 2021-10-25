00be685762bc20d9a0fbe2cd71d95d597bed2164
Ardi Grapci <ardiehnberggrapci@gmail.com>
2021-10-25 10:07:25 +0200

    Create a modal för adding chore

---
+++ components/CreateChore.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Modal, Portal, Text, Button, Provider } from "react-native-paper";
+
+const CreateChore = () => {
+    const [visible, setVisible] = React.useState(false);
+
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+}
+
+const styles = StyleSheet.create({
+    
+})

4fb0a2c1ff1bb4c5306d31ec31f6b4517fd1542f
Ardi Grapci <ardiehnberggrapci@gmail.com>
2021-10-26 15:03:39 +0200

    Create Chore component

--- components/CreateChore.tsx
+++ components/CreateChore.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import * as React from "react";
 import { StyleSheet } from "react-native";
 import { Modal, Portal, Text, Button, Provider } from "react-native-paper";
+import { styles } from "../styles/Styles";
 
 const CreateChore = () => {
     const [visible, setVisible] = React.useState(false);
 
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
+
+    return (
+        <Provider>
+            <Portal>
+                <Modal 
+                    visible={visible}
+                    onDismiss={hideModal}
+                    contentContainerStyle={style.container}
+                >
+                    {/*Add some information to play around with */}
+                </Modal>
+            </Portal>
+            <Button style={styles.buttonContainer} onPress={showModal} >
+                Lägg till ?ö
+            </Button>
+        </Provider>
+    )
 }
 
-const styles = StyleSheet.create({
-    
+export default CreateChore;
+
+const style = StyleSheet.create({
+    container: {
+        padding: 10,
+        borderRadius: 20,
+    }
 })

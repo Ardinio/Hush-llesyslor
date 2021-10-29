a711c591c5d561de5cb9d29a46863bd21a237cb3
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-29 14:41:22 +0200

    Created selector

--- store/task/taskSelectors.ts
+++ store/task/taskSelectors.ts
@@ -1,3 +1,19 @@
-import { RootState } from '../store';
+import { mockedHousehold } from "../../data/householdData";
+import householdReducer from "../household/householdReducer";
+import { selectAllHouseholds } from "../household/householdSelectors";
+import { RootState } from "../store";
 
 export const selectAllTasks = (state: RootState) => state.task.task;
+
+export const selectTasksOnActiveHousehold = (state: RootState) => {
+  return state.task.task.map((task) => {
+    const users = state.user.users.filter(
+      (u) => u.HouseholdId == task.HouseholdId
+    );
+    const currentHousehold = users.map((user) => {
+      const household = mockedHousehold.find((h) => h.Id === user.HouseholdId);
+      return { ...user, household };
+    });
+    return { ...task, users: currentHousehold };
+  });
+};

4285791d513f37fffab1d45eb5c764a7c5d216a4
Mathias Millberg <millberg_m@hotmail.com>
2021-10-29 08:53:30 +0200

    removed account from initialState in AccountState

--- data/accountData.ts
+++ data/accountData.ts
@@ -3,7 +3,7 @@ import { ActiveAccount } from "../entities/ActiveAccount";
 
 export const mockedAccount: ActiveAccount =
   {
-    Id: "1",
-    Email: "test@test.com",
-    isLoggedIn: true,
+    Id: "",
+    Email: "",
+    isLoggedIn: false,
   }

a0c24796cb3e48f69c4ad770518679ac7d7f383b
Ardi Grapci <ardiehnberggrapci@gmail.com>
2021-11-02 13:53:27 +0100

    About to add edit chore

---
+++ components/EditChoreModul.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { View, Text, Modal, TextInput } from "react-native";
+import { styles } from "../styles/Styles";
+import { Button } from "../components";
+import { useState } from "react";
+import RepeatCarousel from "./RepeatCarousel";
+import ValueCarousel from "./ValueCarousel";
+import nextId from "react-id-generator";
+import { useAppDispatch } from "../store/store";
+import { AddTask as EditTask } from "../store/task/taskActions";
+
+function EditChoreModul() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [recurringInDays, setRecurringInDays] = useState<number>();
+  const [energyRequired, setEnergyRequired] = useState<number>();
+  const [errorMsg, setErrorMsg] = React.useState<string>();
+  const dispatch = useAppDispatch();
+
+  const closeModal = () => {
+    setErrorMsg("");
+    setTitle("");
+    setDescription("");
+    setModalVisible(false);
+  };
+
+  const editNewTask = () => {
+    if (!title || !description || !recurringInDays || !energyRequired)
+      return setErrorMsg("Du måste fylla i alla fält");
+
+    dispatch(
+      EditTask({
+        Id: nextId(),
+        HouseholdId: "100",
+        Title: title,
+        Description: description,
+        recurringInDays: recurringInDays!,
+        EnergyRequired: energyRequired!,
+      })
+    );
+    closeModal();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={styles.container}>
+          <View style={styles.modalView2}>
+            <Text>Skapa en ny syssla</Text>
+            <View>
+              <View>
+                <View style={[styles.innerContainer, styles.marginTop]}>
+                  <TextInput
+                    style={styles.textBox}
+                    placeholder="Titel"
+                    placeholderTextColor="grey"
+                    value={title}
+                    onChangeText={(value) => setTitle(value)}
+                  />
+                  <Text style={styles.errorText}>{errorMsg}</Text>
+
+                  <Text style={styles.innerContainerText}></Text>
+                </View>
+              </View>
+
+              <View style={styles.marginTop}>
+                <View style={styles.innerContainer}>
+                  <TextInput
+                    style={styles.textBox}
+                    placeholder="Beskrivning"
+                    placeholderTextColor="grey"
+                    value={description}
+                    onChangeText={(value) => setDescription(value)}
+                  />
+                  <Text style={styles.errorText}>{errorMsg}</Text>
+                </View>
+                <RepeatCarousel
+                  onChangeText={(value) => setRecurringInDays(value)}
+                />
+                <ValueCarousel
+                  onChangeText={(value) => setEnergyRequired(value)}
+                />
+              </View>
+            </View>
+
+            <View style={styles.marginTop}>
+              <Button
+                onPress={editNewTask}
+                buttonTitle="Spara"
+                btnType="window-close"
+              />
+              <View style={styles.marginTop}>
+                <Button
+                  onPress={() => setModalVisible(false)}
+                  buttonTitle="Stäng"
+                  btnType="window-close"
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Button
+        onPress={() => setModalVisible(!modalVisible)}
+        buttonTitle="Ändra"
+        btnType="plus"
+      />
+    </View>
+  );
+}
+
+export default EditChoreModul;

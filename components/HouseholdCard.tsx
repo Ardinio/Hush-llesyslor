53c1b2fd21f96e9ba739a1d64e4087568509c078
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-11-05 13:46:40 +0100

    removed console.log and some css fixes

--- components/HouseholdCard.tsx
+++ components/HouseholdCard.tsx
@@ -6,17 +6,20 @@ import { useAppDispatch } from "../store/store";
 import { styles } from "../styles/Styles";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { EditHousehold } from "../store/household/householdActions";
-import { Household } from '../entities/Household';
+import { Household } from "../entities/Household";
 
 interface Props {
-  household: Household,
-  isowner: boolean,
-  users: any[]
+  household: Household;
+  isowner: boolean;
+  users: any[];
 }
 
 const HouseholdCard = ({ household, isowner, users }: Props) => {
   const dispatch = useAppDispatch();
-  const [editHouseholdNameModalVisibility, setEditHouseholdNameModalVisibility] = React.useState(false);
+  const [
+    editHouseholdNameModalVisibility,
+    setEditHouseholdNameModalVisibility,
+  ] = React.useState(false);
   const [houseHoldName, setHouseHoldName] = React.useState<string>();
   const [houseHoldId, setHouseHoldId] = React.useState<string>("");
   const [errorMsg, setErrorMsg] = React.useState<string>();
@@ -32,59 +35,67 @@ const HouseholdCard = ({ household, isowner, users }: Props) => {
     if (!houseHoldName)
       return setErrorMsg("Fältet med namn för ett hushåll får inte vara tomt!");
     else {
-      const householdWithNewName: Household = { ...household, Name: houseHoldName ?? "" };
+      const householdWithNewName: Household = {
+        ...household,
+        Name: houseHoldName ?? "",
+      };
       dispatch(EditHousehold(householdWithNewName));
       closeModal();
     }
   };
   const { Name, GeneratedCode, Id } = household;
   return (
-      <>
-        <Card style={styles.Card}>
-          <View style={styles.textAlign}>
-            <Text style={styles.title}>{Name}</Text>
-            <TouchableOpacity
-              onPress={() => {
-                  setHouseHoldId(Id);
-                  setHouseHoldName(Name);
-                  setEditHouseholdNameModalVisibility(!editHouseholdNameModalVisibility);
-                }
-              }
-            >
-              {isowner && <MaterialCommunityIcons name="pencil" size={20} />}
-            </TouchableOpacity>
+    <>
+      <Card style={styles.Card}>
+        <View style={styles.textAlign}>
+          <Text style={styles.title}>{Name}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              setHouseHoldId(Id);
+              setHouseHoldName(Name);
+              setEditHouseholdNameModalVisibility(
+                !editHouseholdNameModalVisibility
+              );
+            }}
+          >
+            {isowner && <MaterialCommunityIcons name="pencil" size={20} />}
+          </TouchableOpacity>
+        </View>
+        {users.map(({ Name, avatar, IsOwner }, i) => (
+          <View key={i} style={styles.textAlign}>
+            <Text>{avatar?.Emoji}</Text>
+            <Text>{Name}</Text>
+            <Text style={styles.descriptionText}>
+              {IsOwner ? "(ägare)" : null}
+            </Text>
           </View>
-          {users.map(({ Name, avatar, IsOwner }, i) => (
-            <View key={i} style={styles.textAlign}>
-              <Text>{avatar?.Emoji}</Text>
-              <Text>{Name}</Text>
-              <Text style={styles.descriptionText}>
-                {IsOwner ? "(ägare)" : null}
-              </Text>
-            </View>
-          ))}
-          <Text style={styles.italicFont}>
-            Kod för att gå med i hushållet: {GeneratedCode}
-          </Text>
-        </Card>
+        ))}
+        <Text style={styles.italicFont}>
+          Kod för att gå med i hushållet: {GeneratedCode}
+        </Text>
+      </Card>
 
       <Modal
         animationType="slide"
         transparent={true}
         visible={editHouseholdNameModalVisibility}
         onRequestClose={() => {
-          setEditHouseholdNameModalVisibility(!editHouseholdNameModalVisibility);
+          setEditHouseholdNameModalVisibility(
+            !editHouseholdNameModalVisibility
+          );
         }}
       >
         <View style={styles.container}>
           <View style={styles.modalView}>
             <Text style={styles.buttonText}>Ändra namn på hushåll</Text>
-            <TextInput
-              style={styles.textInputBox}
-              value={houseHoldName}
-              placeholder="Namn på hushållet"
-              onChangeText={(value) => setHouseHoldName(value)}
-            />
+            <View style={[styles.innerContainer, styles.marginTop]}>
+              <TextInput
+                style={styles.textInputBox}
+                value={houseHoldName}
+                placeholder="Namn på hushållet"
+                onChangeText={(value) => setHouseHoldName(value)}
+              />
+            </View>
             <Text style={styles.errorText}>{errorMsg}</Text>
             <View style={styles.buttonsContainer}>
               <View style={styles.iconWrapper}>
